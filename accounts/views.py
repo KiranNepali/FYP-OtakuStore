e06a55273d30ad0a2e046b28a4210cb1bdead9e7
Kiran Nepali <itsmekiran44@gmai.com>
2023-03-28 21:43:22 +0545

    register model forms and view making

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from .import views
+from .forms import RegistrationForm
+from .models import Account
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # fetch all the fields from this request post
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
+            # make user object
+            user = Account.objects.create_user(
+                first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+
+
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
 
 def login(request):
     return render(request, 'accounts/login.html')
